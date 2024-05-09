7dee3eebe23e757aa63f75d681d5b2a4974b4d48
ericschmidutz <e-schmidutz@onu.edu>
2024-05-08 21:19:00 -0400

    isLeapYear is implemented

--- main.cpp
+++ main.cpp
@@ -15,3 +15,22 @@ int main() {
 
 	return 0;
 }
+
+bool isLeapYear(int year) {
+	
+	if (year < 1582) {
+		throw (1582);
+	}
+	else if (year % 100 == 0) {
+		if (year % 400 == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	else if (year % 4 == 0) {
+		return true;
+	}
+	return false;
+}
