ab8e846c5cb3eded633268e6fa740ac4ba6a996e
ericschmidutz <e-schmidutz@onu.edu>
2024-05-08 21:14:23 -0400

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+bool isLeapYear(int year);
+/**
+isLeapYear – determines whether a given year is a leap year
+under the Gregorian calendar.
+@param year the year; expected to be >= 1582
+@return true if year is a leap year; false otherwise
+*/
+
+int main() {
+
+	return 0;
+}
